59e743b031925c878915376e4b949acd5beb2a63
Cristian Vargas <cristian@swapps.co>
2016-07-28 14:52:07 -0500

    Add method to retrieve user detail

--- balystic/client.py
+++ balystic/client.py
@@ -8,7 +8,7 @@ class Client(object):
     Encapsulates all the logic to consume the services provided
     by the balystic API.
     """
-    self.USER_ENDPOINT = 'users/'
+    USER_ENDPOINT = 'users/'
 
     def __init__(self, token, root):
         """
@@ -16,10 +16,10 @@ class Client(object):
         Root must be the full path to the api root
         i.e. http://sample.7dhub.com/api/
         """
-        self.token = token
+        self.headers = {'Authorization': 'TOKEN ' + token}
         self.root = root
 
-    def _make_request(self, path, headers, method):
+    def _make_request(self, path, method):
         """
         Encapsulates error handling. Sets an standard way to handle
         requests across the client.
@@ -33,16 +33,22 @@ class Client(object):
             request_method = requests.delete
         full_path = self.root + path
         try:
-            response = request_method(full_path, headers=headers)
+            response = request_method(full_path, headers=self.headers)
             return response.json()
         except:
             pass
 
-    def get_users():
+    def get_users(self):
         """
         Retrieves the list of users in the community.
         There are two kind of users, owners and regular users.
         """
-        response = self._make_request(self.USER_ENDPOINT, {}, 'GET')
-        print response
-        return response
+        return self._make_request(self.USER_ENDPOINT, 'GET')
+
+    def get_user_detail(self, username):
+        """
+        Retrieves an user detail.
+        The user must be in the community for this to work.
+        """
+        return self._make_request(
+            self.USER_ENDPOINT + username + '/', 'GET')
