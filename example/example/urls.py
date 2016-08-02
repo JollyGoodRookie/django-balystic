d75da3f54554b0bf2a7fe61b292832d2728a0fd1
Cristian Vargas <cristian@swapps.co>
2016-08-02 17:05:33 -0500

    Users can update their profile information

--- example/example/urls.py
+++ example/example/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('balystic.urls', namespace='balystic')),
+    url(r'', include('balystic.urls')),
 ]
